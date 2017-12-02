4b7ab4c39554c54effe2d2013119b79d296b4023
Ilya Builuk <ilya.builuk@gmail.com>
2017-12-02 14:21:40 +0100

    Add lfs bootscripts

---
+++ scripts/build/7.2-make-lfs-bootscripts.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+echo "Building lfs bootscripts.."
+echo "Approximate build time: less than 0.1 SBU"
+echo "Required disk space: 244 KB"
+
+# 7.2. LFS-Bootscripts package contains a set of scripts to start/stop
+# the LFS system at bootup/shutdown
+tar -xf /sources/lfs-bootscripts-*.tar.bz2 -C /tmp/ \
+  && mv /tmp/lfs-bootscripts-* /tmp/lfs-bootscripts \
+  && pushd /tmp/lfs-bootscripts
+
+make install
+
+# cleanup
+popd \
+  && rm -rf /tmp/lfs-bootscripts
--- scripts/build/as-chroot-with-usr.sh
+++ scripts/build/as-chroot-with-usr.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 set -e
 echo "Continue with chroot environment.."
+
+# configure system
+sh /tools/7.2-make-lfs-bootscripts.sh
