ce8f630faba61ad13b8b18ce2a9d62dfbac71210
reinterpretcat <ilya.builuk@gmail.com>
2017-12-09 22:48:25 +0100

    Modify fstab to use /dev/ram
    This setting will be needed by bootable iso image

--- scripts/build/8.2-create-fstab.sh
+++ scripts/build/8.2-create-fstab.sh
@@ -7,8 +7,7 @@ cat > /etc/fstab << "EOF"
 # file system   mount-point   type      options               dump  fsck
 #                                                                   order
 
-/dev/sda1       /             ext4      defaults              1     1
-#/dev/sda2      /swap         swap      pri=1                 0     0
+/dev/ram        /             auto      defaults              1     1
 proc            /proc         proc      nosuid,noexec,nodev   0     0
 sysfs           /sys          sysfs     nosuid,noexec,nodev   0     0
 devpts          /dev/pts      devpts    gid=5,mode=620        0     0
